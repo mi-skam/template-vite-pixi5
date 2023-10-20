930d27fdb70b493ce1d2d3a9c13d51d40eb8c531
mi-skam <maksim.codes@mailbox.org>
2023-10-20 10:04:32 +0200

    Adding assetsInclude configuration
    For pixi5 projects it's customary to add assets, thus we need to inform vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 export default defineConfig({
   plugins: [
     eslintPlugin({
+      assetsInclude: ['**/*.mp3', '**/*.jpeg', '**/*.png', '**/*.task'],
       cache: false,
     }),
   ],
